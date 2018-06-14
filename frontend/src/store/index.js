863ecc72241832a15eefeb47fce302adbc52c156
Pablo Satler <pablosatler@gmail.com>
2018-06-14 16:53:30 -0300

    refactor: change compose in create store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -7,13 +7,22 @@ import rootReducer from '../reducers';
 
 const middleware = [thunk];
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
-//   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(...middleware)
   )
-);
+)
+
+// const store = createStore(
+//   rootReducer,
+// //   initialState,
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
 
 export default store;
